d47c5a66052d274a12964a2fadb7f116709dcacd
Jahangeer Ansari <jahangeeransari194@gmail.com>
2025-06-18 10:36:14 +0530

    doted seprator created with signin-card

---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,29 @@
+import { DottedSeparator } from "@/components/dotted-separator";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+export const SignInCard = () => {
+  return (
+    <Card className="w-full h-full md:w-[487px] border-none shadow-none">
+      <CardHeader className="flex items-center justify-center text-center p-7">
+        <CardTitle className="text-2xl">Welcome Back...</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+       
+      </CardHeader>
+      <div className="px-7 mb-2">
+        <DottedSeparator/>
+      </div>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+};
