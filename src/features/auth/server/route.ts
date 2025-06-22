b1f949bcd47bcade14faa46dbcb1e9e40a8eddfe
Jahangeer Ansari <jahangeeransari194@gmail.com>
2025-06-22 11:39:33 +0530

    login and logout appwrite base authenitaciton completed

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -3,35 +3,43 @@ import { zValidator } from "@hono/zod-validator";
 import { loginSchema, registerSchema } from "../schemas";
 import { createAdminClient } from "@/lib/appwrite";
 import { ID } from "node-appwrite";
-import {setCookie} from "hono/cookie"
+import { setCookie ,deleteCookie} from "hono/cookie";
 import { AUTH_COOKIES } from "../constants";
-const app = new Hono().post(
-  "/login",
-  zValidator( "json", loginSchema),(c) => {
-    const {email,password} = c.req.valid("json");
+import { success } from "zod/v4";
+
+const app = new Hono()
+  .post("/login", zValidator("json", loginSchema), async (c) => {
+    const { email, password } = c.req.valid("json");
+    const { account } = await createAdminClient();
+    const session = await account.createEmailPasswordSession(email, password);
+    setCookie(c, AUTH_COOKIES, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
     return c.json({
-      email,password
-    })
-  }
-).post("/register",zValidator("json",registerSchema),async (c)=>{
-  const {email,name,password} = c.req.valid("json");
-  const {account} = await createAdminClient();
-  const user = await account.create(
-    ID.unique(),
-    email,
-    password,
-    name
-  );
-  const session = await account.createEmailPasswordSession(email,password)
-setCookie(c,AUTH_COOKIES,session.secret,{
-  path:"/",
-  httpOnly:true,
-  secure:true,
-  sameSite:"strict",
-  maxAge:60 * 60 * 24 * 30
-})
-  return c.json({
-    data:user
+      success: true,
+    });
   })
-})
+  .post("/register", zValidator("json", registerSchema), async (c) => {
+    const { email, name, password } = c.req.valid("json");
+    const { account } = await createAdminClient();
+    await account.create(ID.unique(), email, password, name);
+    const session = await account.createEmailPasswordSession(email, password);
+    setCookie(c, AUTH_COOKIES, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+    return c.json({
+      success: true,
+    });
+  }).post("/logout",(c) =>{
+    deleteCookie(c,AUTH_COOKIES);
+    return c.json({success:true})
+  })
 export default app;
