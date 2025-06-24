bda27d9895548a5bca2a0f4872021b22aef4f635
Jahangeer Ansari <jahangeeransari194@gmail.com>
2025-06-24 13:18:02 +0530

    sidebar created for the dashboard

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,11 +1,10 @@
 import { getCurrent } from "@/features/auth/actions";
 
 import { UserButton } from "@/features/auth/components/user-button";
-import { redirect, useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const user = await getCurrent();
-
   if (!user) {
     redirect("/sign-in");
   }
