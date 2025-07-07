808c5c22eccf4edb0dfde582fc7be37bea2f7685
Jahangeer Ansari <jahangeeransari194@gmail.com>
2025-07-07 13:39:16 +0530

    image upload completed for appWrite

--- src/features/workspaces/api/use-create-workspace.ts
+++ src/features/workspaces/api/use-create-workspace.ts
@@ -4,15 +4,17 @@ import {client} from "@/lib/rpc";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
+
 type ResponseType = InferResponseType<typeof client.api.workspaces["$post"]>;
-type RequestType = InferRequestType<typeof client.api.workspaces["$post"]>["json"];
+type RequestType = InferRequestType<typeof client.api.workspaces["$post"]>;
 
 export const useCreateWorkspace = () => {
+  
     const router = useRouter();
     const queryClient = useQueryClient();
     const mutation = useMutation<ResponseType, Error, RequestType>({
-      mutationFn: async (json) => {
-        const response = await client.api.workspaces["$post"]({ json });
+      mutationFn: async ({form}) => {
+        const response = await client.api.workspaces["$post"]({form});
         return await response.json();
       },
       onSuccess: () => {
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,24 +2,34 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASES_ID, WORKSPACES_ID } from "@/config";
+import { DATABASES_ID, IMAGES_ID, WORKSPACES_ID } from "@/config";
 import { ID } from "node-appwrite";
+
 
 const app = new Hono().post(
   "/",
-  zValidator("json", createWorkspaceSchema),
+  zValidator("form", createWorkspaceSchema),
   sessionMiddleware,
   async (c) => {
     const databases = c.get("databases");
+    const storage = c.get("storage");
     const user = c.get("user");
-    const { name } = c.req.valid("json");
+    const { name, image } = c.req.valid("form");
+    let uploadImageUrl: string | undefined;
+    if (image instanceof File) {
+      const file = await storage.createFile(IMAGES_ID, ID.unique(), image);
+      const arrayBuffer = await storage.getFileDownload(IMAGES_ID, file.$id);
+      uploadImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`
+    }
+    
     const workspace = await databases.createDocument(
       DATABASES_ID,
       WORKSPACES_ID,
       ID.unique(),
         {
             name,
-          userId:user.$id
+          userId: user.$id,
+          imageUrl:uploadImageUrl
        }
       );
       return c.json({data:workspace})
