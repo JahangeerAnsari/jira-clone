028baba3f5ce0c500827063ebfecf85a696e672e
Jahangeer Ansari <jahangeeransari194@gmail.com>
2025-07-09 12:34:07 +0530

    we have create new workspace create page once new user or the user dont have any workspace will redirect to create workspaces page

---
+++ src/app/(standalone)/workspaces/create/page.tsx
@@ -0,0 +1,11 @@
+import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
+
+const CreateWorksapcePage = () => {
+    return (
+      <div className="w-full lg:max-w-xl">
+        <CreateWorkspaceForm/>
+      </div>
+    );
+}
+ 
+export default CreateWorksapcePage;
--- src/components/workspace-swither.tsx
+++ src/components/workspace-swither.tsx
@@ -35,7 +35,7 @@ export const WorkspaceSwitcher = () => {
           <SelectValue placeholder="No Workpsace selected " />
         </SelectTrigger>
         <SelectContent>
-          {workspaces?.documents.map((workspace) => (
+          {workspaces?.documents?.map((workspace) => (
             <SelectItem key={workspace.$id} value={workspace.$id}>
               <div className="flex justify-start items-center gap-3 font-medium">
                 <WorkpsaceAvatar
