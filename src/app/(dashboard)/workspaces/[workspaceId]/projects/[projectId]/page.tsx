4e94a2b5f8c868247839260f19dc925b5318f769
Jahangeer Ansari <jahangeeransari194@gmail.com>
2025-07-16 13:00:29 +0530

    fetched indivisual project data api created

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,10 +1,21 @@
+import { getCurrent } from "@/features/auth/queries";
+import { getProject } from "@/features/projects/queries";
+import { redirect } from "next/navigation";
+
 interface ProjectIdPageProps {
   params: {
     projectId: string;
   };
 }
-const ProjectIdPage = ({ params }: ProjectIdPageProps) => {
-  return <div>{params.projectId}</div>;
+const ProjectIdPage = async ({ params }: ProjectIdPageProps) => {
+    const user = await getCurrent();
+    const initialValues = await getProject({
+        projectId:params.projectId
+    })
+    if (!user) {
+        redirect("/sign-in")
+    }
+    return <div>{JSON.stringify(initialValues)}</div>;
 };
 
 export default ProjectIdPage;
---
+++ src/features/projects/queries.ts
@@ -0,0 +1,32 @@
+import { createSessionClient } from "@/lib/appwrite";
+import { getMember } from "../members/utils";
+import { DATABASES_ID, PROJECT_ID } from "@/config";
+import { Project } from "./types";
+
+interface ProjectProps {
+  projectId: string;
+}
+export const getProject = async ({ projectId }: ProjectProps) => {
+  try {
+    const { account, databases } = await createSessionClient();
+      const user = await account.get();
+      const project = await databases.getDocument<Project>(
+        DATABASES_ID,
+        PROJECT_ID,
+        projectId
+      );
+    const member = await getMember({
+      databases,
+      userId: user.$id,
+      workspaceId:project.workspaceId,
+    });
+    if (!member) {
+      return null;
+    }
+    
+
+    return project;
+  } catch (error) {
+    return null;
+  }
+};
