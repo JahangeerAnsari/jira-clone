0ebf7e6c876f54c568ecac60eee58b39c5ef7eca
Jahangeer Ansari <jahangeeransari194@gmail.com>
2025-07-21 08:11:22 +0530

    api created for create task and get the task for project

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@ export const DATABASES_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!;
 export const WORKSPACES_ID = process.env.NEXT_PUBLIC_APPWRITE_WORKSPACES_ID!;
 export const MEMBER_ID = process.env.NEXT_PUBLIC_APPWRITE_MEMBER_ID!;
 export const PROJECT_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!;
+export const TASK_ID = process.env.NEXT_PUBLIC_APPWRITE_TASK_ID!;
 export const IMAGES_ID = process.env.NEXT_PUBLIC_APPWRITE_IMAGES_BUCKET_ID!;
---
+++ src/features/tasks/types.ts
@@ -0,0 +1,7 @@
+export enum TaskStatus {
+  BACKLOG = "BACKLOG",
+  TODO = "TODO",
+  IN_PROGRESS = "IN_PROGRESS",
+  PR_REVIEW = "PR_REVIEW",
+  DONE = "DONE",
+}
