fc0a6fb712f6bef0d6c6f4484f449f05dbb00776
Jahangeer Ansari <jahangeeransari194@gmail.com>
2025-08-04 00:02:08 +0530

    bug fix for get workspaces

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -4,17 +4,16 @@ import { redirect } from "next/navigation";
 
 export default async function Home() {
   const user = await getCurrent();
-  const workspaces = await getWorkspaces();
 
   if (!user) {
     redirect("/sign-in");
   }
-  // if there is no workspace redirect to create workspace page
-  if (workspaces.total === 0) {
+
+  const workspaces = await getWorkspaces();
+
+  if (workspaces.total === 0 || !workspaces.document?.[0]?.$id) {
     redirect("/workspaces/create");
-  } else {
-    // else redirect to the perticular workspace
-    redirect(`/workspaces/${workspaces?.documents[0].$id}`);
   }
-  return <div className="bg-neutral-500 p-4 h-full"></div>;
+
+  redirect(`/workspaces/${workspaces.document[0].$id}`);
 }
