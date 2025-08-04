7c8e6665c589636b7042344b6ff619b81939e402
Jahangeer Ansari <jahangeeransari194@gmail.com>
2025-08-04 10:00:08 +0530

    client fixed

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -6,16 +6,18 @@ import members from "@/features/members/server/route";
 import projects from "@/features/projects/server/route";
 import tasks from "@/features/tasks/server/route";
 
-const app = new Hono().basePath("/api");
- app
+const app = new Hono()
+  .basePath("/api")
   .route("/auth", auth)
   .route("/workspaces", workspaces)
   .route("/members", members)
-.route("/projects", projects)
-  .route("/tasks", tasks);
+  .route("/projects", projects)
+  .route("/tasks", tasks); // ✅ <- now `app` has full type
+
 export const GET = handle(app);
 export const POST = handle(app);
 export const PATCH = handle(app);
 export const DELETE = handle(app);
-export type AppType = typeof app;
 
+// ✅ AppType now gets full route types
+export type AppType = typeof app;
